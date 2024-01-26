d020b4430d78b2ad49ea96d726ee8084cd19174d
firdausnanda <firdausnanda46@gmail.com>
2024-01-26 09:01:19 +0700

    fix: menu

--- resources/views/layouts/dashboard/menu.blade.php
+++ resources/views/layouts/dashboard/menu.blade.php
@@ -4,42 +4,52 @@
       <ul class="sidebar-nav" id="sidebar-nav">
 
           <li class="nav-item">
-              <a class="nav-link {{ request()->routeIs('admin.index') ? '' : 'collapsed' }}" href="{{ route('admin.index') }}">
+              <a class="nav-link {{ request()->routeIs('admin.index') ? '' : 'collapsed' }}"
+                  href="{{ route('admin.index') }}">
                   <i class="bi bi-grid"></i>
                   <span>Dashboard</span>
               </a>
           </li><!-- End Dashboard Nav -->
 
           <li class="nav-item">
-              <a class="nav-link {{ request()->routeIs('admin.ebook.*') ? '' : 'collapsed' }}" href="{{ route('admin.ebook.index') }}">
+              <a class="nav-link {{ request()->routeIs('admin.ebook.*') ? '' : 'collapsed' }}"
+                  href="{{ route('admin.ebook.index') }}">
                   <i class="fa-solid fa-book"></i>
                   <span>E Book</span>
               </a>
           </li><!-- End Dashboard Nav -->
 
           <li class="nav-item">
-              <a class="nav-link {{ request()->routeIs('admin.jurnal.*') ? '' : 'collapsed' }}" href="{{ route('admin.jurnal.index') }}">
+              <a class="nav-link {{ request()->routeIs('admin.jurnal.*') ? '' : 'collapsed' }}"
+                  href="{{ route('admin.jurnal.index') }}">
                   <i class="fa-solid fa-book-journal-whills"></i>
                   <span>Jurnal</span>
               </a>
           </li><!-- End Dashboard Nav -->
 
           <li class="nav-item">
-              <a class="nav-link collapsed" href="index.html">
+              <a class="nav-link collapsed" href="#">
+                  <i class="fa-solid fa-table-list"></i>
+                  <span>Kategori</span>
+              </a>
+          </li><!-- End Dashboard Nav -->
+
+          <li class="nav-item">
+              <a class="nav-link collapsed" href="#">
                   <i class="fa-solid fa-bars"></i>
                   <span>Menu</span>
               </a>
           </li><!-- End Dashboard Nav -->
 
           <li class="nav-item">
-              <a class="nav-link collapsed" href="index.html">
+              <a class="nav-link collapsed" href="#">
                   <i class="fa-solid fa-network-wired"></i>
                   <span>IP Local</span>
               </a>
           </li><!-- End Dashboard Nav -->
 
           <li class="nav-item">
-              <a class="nav-link collapsed" href="index.html">
+              <a class="nav-link collapsed" href="#">
                   <i class="fa-solid fa-users"></i>
                   <span>Pengguna</span>
               </a>
@@ -48,21 +58,21 @@
           <li class="nav-heading">Monitoring</li>
 
           <li class="nav-item">
-              <a class="nav-link collapsed" href="index.html">
+              <a class="nav-link collapsed" href="#">
                   <i class="fa-solid fa-list-check"></i>
                   <span>Riwayat Aktivitas</span>
               </a>
           </li><!-- End Dashboard Nav -->
 
           <li class="nav-item">
-              <a class="nav-link collapsed" href="index.html">
+              <a class="nav-link collapsed" href="#">
                   <i class="fa-solid fa-database"></i>
                   <span>Backup Database</span>
               </a>
           </li><!-- End Dashboard Nav -->
 
           <li class="nav-item">
-              <a class="nav-link collapsed" href="index.html">
+              <a class="nav-link collapsed" href="#">
                   <i class="fa-solid fa-bug"></i>
                   <span>Error Log</span>
               </a>
