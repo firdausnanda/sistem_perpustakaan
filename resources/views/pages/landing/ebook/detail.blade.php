d7bb44f29da4ef31ac9d2eeea4d15ad07780e236
firdausnanda <firdausnanda46@gmail.com>
2024-01-29 12:38:16 +0700

    fix: preview

--- resources/views/pages/landing/ebook/detail.blade.php
+++ resources/views/pages/landing/ebook/detail.blade.php
@@ -66,7 +66,7 @@
         <div class="modal-dialog modal-xl" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="modalTitleId">Panduan</h5>
+                    <h5 class="modal-title" id="modalTitleId">Preview</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
