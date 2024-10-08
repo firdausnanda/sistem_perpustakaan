6d69b221790dd3537a1ca0a65135f91ca6763d85
firdausnanda <firdausnanda46@gmail.com>
2024-10-08 13:10:45 +0700

    fix: sticky button

--- resources/views/layouts/landing/main.blade.php
+++ resources/views/layouts/landing/main.blade.php
@@ -2,39 +2,58 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Perpustakaan - ITSK RS dr. Soepraoen</title>
-  <meta content="E-Book & E-Journal ITSK Soepraoen" name="description">
-  <meta content="E-Book & E-Journal ITSK Soepraoen" name="keywords">
+    <title>Perpustakaan - ITSK RS dr. Soepraoen</title>
+    <meta content="E-Book & E-Journal ITSK Soepraoen" name="description">
+    <meta content="E-Book & E-Journal ITSK Soepraoen" name="keywords">
 
-  <!-- Favicons -->
-  <link href="{{ asset('img/logo.png') }}" rel="icon">
-  <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+    <!-- Favicons -->
+    <link href="{{ asset('img/logo.png') }}" rel="icon">
+    <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com" rel="preconnect">
-  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
-    rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+        rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/aos/aos.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/fontawesome/css/fontawesome.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/fontawesome/css/brands.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/fontawesome/css/solid.css') }}" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/aos/aos.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/fontawesome/css/fontawesome.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/fontawesome/css/brands.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/fontawesome/css/solid.css') }}" rel="stylesheet">
 
 
-  <!-- Template Main CSS File -->
-  <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <!-- Template Main CSS File -->
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 
-  <!-- =======================================================
+    <style>
+        .scroll-wa {
+            position: fixed;
+            right: 15px;
+            bottom: 75px;
+            z-index: 99999;
+            width: 44px;
+            height: 44px;
+            border-radius: 50px;
+            transition: all 0.4s;
+            background-color: #20c997;
+        }
+
+        .scroll-wa:hover {
+            background-color: color-mix(in srgb, #20c997, transparent 20%);
+            color: var(--contrast-color);
+        }
+    </style>
+
+    <!-- =======================================================
   * Template Name: Append
   * Updated: Sep 18 2023 with Bootstrap v5.3.2
   * Template URL: https://bootstrapmade.com/append-bootstrap-website-template/
@@ -45,42 +64,46 @@
 
 <body class="starter-page-page" data-bs-spy="scroll" data-bs-target="#navmenu">
 
-  @include('layouts.landing.header')
+    @include('layouts.landing.header')
 
-  <main id="main">
-    @yield('content')
-  </main>
+    <main id="main">
+        @yield('content')
+    </main>
 
-  @include('layouts.landing.footer')
+    @include('layouts.landing.footer')
 
-  <!-- Scroll Top Button -->
-  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
-      class="bi bi-arrow-up-short"></i></a>
+    <!-- Scroll Top Button -->
+    <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Preloader -->
-  <div id="preloader">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-  </div>
+    <a href="https://wa.me/6282229829298" target="_blank"
+        class="scroll-wa d-flex align-items-center justify-content-center">
+        <i class="bi bi-whatsapp text-white"></i></a>
 
-  <!-- Vendor JS Files -->
-  <script src="{{ asset('vendor/jquery/jquery-3.6.0.min.js') }}"></script>
-  <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('vendor/glightbox/js/glightbox.min.js') }}"></script>
-  <script src="{{ asset('vendor/purecounter/purecounter_vanilla.js') }}"></script>
-  <script src="{{ asset('vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-  <script src="{{ asset('vendor/swiper/swiper-bundle.min.js') }}"></script>
-  <script src="{{ asset('vendor/aos/aos.js') }}"></script>
-  <script src="{{ asset('vendor/php-email-form/validate.js') }}"></script>
-  <script src="{{ asset('vendor/loading-overlay/loadingoverlay.min.js') }}"></script>
-  <script src="{{ asset('vendor/sweetalert/sweetalert.all.js') }}"></script>
+    <!-- Preloader -->
+    <div id="preloader">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 
-  <!-- Template Main JS File -->
-  <script src="{{ asset('js/main.js') }}"></script>
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('vendor/jquery/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('vendor/purecounter/purecounter_vanilla.js') }}"></script>
+    <script src="{{ asset('vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/aos/aos.js') }}"></script>
+    <script src="{{ asset('vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('vendor/loading-overlay/loadingoverlay.min.js') }}"></script>
+    <script src="{{ asset('vendor/sweetalert/sweetalert.all.js') }}"></script>
 
-  @yield('script')
+    <!-- Template Main JS File -->
+    <script src="{{ asset('js/main.js') }}"></script>
+
+    @yield('script')
 
 </body>
 
